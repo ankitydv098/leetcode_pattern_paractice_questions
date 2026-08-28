a3570cf222f672fbacb9a31fea60b2ec70baddc0
Ankit Kumar <ankitydv098@gmail.com>
2026-08-28 20:00:50 +0530

    Time: 0 ms (100%), Space: 44.3 MB (30.85%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -4,38 +4,54 @@ class Solution {
             return head;
         }
 
-        int len = 0;
-        ListNode l = head;
+        // int len = 0;
+        // ListNode l = head;
 
-        while(l != null){
-            len++;
-            l=l.next;
-        }
-        int x = 0;
-        ListNode[] arr = new ListNode[len];
-        l = head; 
-        while(l != null){
-            arr[x] = l;
-            l=l.next;
-            x++;
-        }
+        // while(l != null){
+        //     len++;
+        //     l=l.next;
+        // }
+        // int x = 0;
+        // ListNode[] arr = new ListNode[len];
+        // l = head; 
+        // while(l != null){
+        //     arr[x] = l;
+        //     l=l.next;
+        //     x++;
+        // }
 
-        int start = 0,
-            end= len-1;
+        // int start = 0,
+        //     end= len-1;
 
-        while(start<end){
-            ListNode temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] =temp;
-            start++;
-            end--;
-        }
-        for(int k=0;k<len-1;k++){
-            arr[k].next = arr[k+1];
-        }
-        arr[len-1].next  = null;
+        // while(start<end){
+        //     ListNode temp = arr[start];
+        //     arr[start] = arr[end];
+        //     arr[end] =temp;
+        //     start++;
+        //     end--;
+        // }
+        // for(int k=0;k<len-1;k++){
+        //     arr[k].next = arr[k+1];
+        // }
+        // arr[len-1].next  = null;
 
-        return arr[0];
+        // return arr[0];
         
+        ListNode prev = null;
+        ListNode curr = head;
+         ListNode next_node;
+        while(curr!=null){
+             next_node = curr.next;
+             curr.next = prev;
+              prev = curr;
+             curr =  next_node;
+        }
+        
+        
+       
+        return prev;
+
+
+
     }
 }
